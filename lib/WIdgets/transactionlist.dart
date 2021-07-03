be4902de0d0c7ba1114fe368f7c83c9773cdb9d1
Yeasir Arefin Tusher <tusher15-5944@diu.edu.bd>
2021-07-03 18:10:54 +0600

    ListTile widget applied on the lists of transactionlist.dart file
    Learned how listTile widget works. Just put the elements into Leading, Title, Subtitle, Trailing and see the magic. verry usefull widget

--- lib/WIdgets/transactionlist.dart
+++ lib/WIdgets/transactionlist.dart
@@ -36,48 +36,71 @@ class transactionList extends StatelessWidget {
               itemCount: ls.length,
               itemBuilder: (BuildContext context, int index) {
                 return Card(
-                  shadowColor: Colors.grey,
-                  elevation: 2,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.fromLTRB(15, 5, 35, 5),
-                        padding: EdgeInsets.all(8),
-                        child: Text(
-                          ls[index].amount.toStringAsFixed(2) + '৳',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          border: Border.all(
-                            color: Colors.orange,
-                            style: BorderStyle.solid,
-                            width: 1,
+                  margin: EdgeInsets.symmetric(vertical: 6, horizontal: 5),
+                  elevation: 4,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.orange.shade400,
+                      radius: 35,
+                      child: Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: FittedBox(
+                          child: Text(
+                            '${ls[index].amount}৳',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
                           ),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            ls[index].title,
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Text(DateFormat.yMd().format(ls[index].date),
-                              style: TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey))
-                        ],
-                      )
-                    ],
+                    ),
+                    title: Text(ls[index].title),
+                    subtitle: Text(DateFormat.yMMMMd().format(ls[index].date)),
                   ),
                 );
+                // //Card(
+                //   shadowColor: Colors.grey,
+                //   elevation: 2,
+                //   child: Row(
+                //     mainAxisAlignment: MainAxisAlignment.start,
+                //     children: <Widget>[
+                //       Container(
+                //         margin: EdgeInsets.fromLTRB(15, 5, 35, 5),
+                //         padding: EdgeInsets.all(8),
+                //         child: Text(
+                //           ls[index].amount.toStringAsFixed(2) + '৳',
+                //           style: TextStyle(
+                //               color: Colors.black,
+                //               fontSize: 20,
+                //               fontWeight: FontWeight.bold),
+                //         ),
+                //         decoration: BoxDecoration(
+                //           borderRadius: BorderRadius.circular(5),
+                //           border: Border.all(
+                //             color: Colors.orange,
+                //             style: BorderStyle.solid,
+                //             width: 1,
+                //           ),
+                //         ),
+                //       ),
+                //       Column(
+                //         crossAxisAlignment: CrossAxisAlignment.start,
+                //         children: <Widget>[
+                //           Text(
+                //             ls[index].title,
+                //             style: TextStyle(
+                //                 fontSize: 16, fontWeight: FontWeight.bold),
+                //           ),
+                //           Text(DateFormat.yMd().format(ls[index].date),
+                //               style: TextStyle(
+                //                   fontSize: 14,
+                //                   fontWeight: FontWeight.bold,
+                //                   color: Colors.grey))
+                //         ],
+                //       )
+                //     ],
+                //   ),
+                // );
               },
             ),
           );
