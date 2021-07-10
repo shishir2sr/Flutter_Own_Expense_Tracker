feb2c48d11889882b45405be8704cbb47faf0235
Yeasir Arefin Tusher <tusher15-5944@diu.edu.bd>
2021-07-10 18:33:47 +0600

    Improvement: Performance By Const Widget
    Using Const property to imporve performance

--- lib/WIdgets/chart_bar.dart
+++ lib/WIdgets/chart_bar.dart
@@ -6,7 +6,7 @@ class ChartBar extends StatelessWidget {
   final double spendingAmount;
   final double spendingAmountpct;
 
-  ChartBar(this.label, this.spendingAmount, this.spendingAmountpct);
+  const ChartBar(this.label, this.spendingAmount, this.spendingAmountpct);
 
   @override
   Widget build(BuildContext context) {
